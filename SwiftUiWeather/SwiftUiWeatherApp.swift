bb9566d1b0b45ee6b200afc48f0e4e27cec8fec0
Stephen  Chacha <48447675+stevechacha@users.noreply.github.com>
2024-09-21 02:09:27 +0300

    Initial Commit

---
+++ SwiftUiWeather/SwiftUiWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiWeatherApp.swift
+//  SwiftUiWeather
+//
+//  Created by stephen chacha on 21/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
